a553d50ac51796b7719da6777a0890ea464f0948
julianalexgo <julianalexgo@gmail.com>
2025-06-07 12:33:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import plotly.express as px
 import streamlit as st
 
 #Streamlit configuration
-st.set_page_config(page_title = "Fetch Stocks")
+st.set_page_config(page_title = "FetchStocks")
 
 #Function to Save to CSV
 def fetch_stocks(ticker, syear, smonth, sday, eyear, emonth, eday):
@@ -22,8 +22,8 @@ def fetch_stocks(ticker, syear, smonth, sday, eyear, emonth, eday):
 with st.form('ticker_form'):
     st.write("Enter the ticker symbol and date range to fetch stock data.")
     ticker = st.text_input("Enter the ticker symbol:", key = 'ticker_name').upper()
-    sdate = st.text_input("Enter start date (MM/DD/YYYY): ", key = 'start_date')
-    edate = st.text_input("Enter end date (MM/DD/YYYY): ", key = 'end_date')
+    sdate = st.date_input("Select date range:", key = 'sdate', value = None, format = "MM/DD/YYYY", min_value = dt.date(1900, 1, 1), max_value = 'today')
+    edate = st.date_input("Select date range:", key = 'edate', value = None, format = "MM/DD/YYYY", min_value = sdate, max_value = 'today')
     submitted = st.form_submit_button("Fetch Data")
 
 #Get inputs before splitting the code
@@ -34,17 +34,15 @@ with st.form('ticker_form'):
 
 if submitted:
     st.success(f"Fetching data for {ticker} from {sdate} to {edate}...")
-    if ticker and sdate and edate:
+    if (ticker or sdate or edate) != None:
         try:
-            smonth, sday, syear = map(int, sdate.split('/'))
-            emonth, eday, eyear = map(int, edate.split('/'))
-            df = fetch_stocks(ticker, syear, smonth, sday, eyear, emonth, eday)
+            df = fetch_stocks(ticker, sdate.year, sdate.month, sdate.day, edate.year, edate.month, edate.day)
             st.dataframe(df)
             fig = px.line(df, x = df.index, y = df.High) #Reference them like this or like df['Close'] but refer to index this way
             st.plotly_chart(fig)
         except Exception as e:
             print(f"An error occurred: {e}")
-    elif not ticker or not sdate or not edate:
+    elif (ticker or sdate or edate) is None:
         st.warning("Please enter a ticker symbol and dates to fetch data.")
 
 
