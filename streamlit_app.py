623f12973c840a3df1da60a08363cf03353ed894
julianalexgo <julianalexgo@gmail.com>
2025-06-07 12:40:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ with st.form('ticker_form'):
     st.write("Enter the ticker symbol and date range to fetch stock data.")
     ticker = st.text_input("Enter the ticker symbol:", key = 'ticker_name').upper()
     sdate = st.date_input("Select start date:", key = 'sdate', value = None, format = "MM/DD/YYYY", min_value = dt.date(1900, 1, 1), max_value = 'today')
-    edate = st.date_input("Select end date:", key = 'edate', value = None, format = "MM/DD/YYYY", min_value = sdate, max_value = 'today')
+    edate = st.date_input("Select end date:", key = 'edate', value = 'today', format = "MM/DD/YYYY", min_value = sdate, max_value = 'today')
     submitted = st.form_submit_button("Fetch Data")
 
 #Get inputs before splitting the code
