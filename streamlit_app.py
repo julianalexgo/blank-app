4d6b482a2b93fcc77bcfea3250eb9dc777a85b4e
julianalexgo <julianalexgo@gmail.com>
2025-06-06 23:54:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ with st.form('ticker_form'):
     ticker = st.text_input("Enter the ticker symbol:", key = 'ticker_name').upper()
     sdate = st.text_input("Enter start date (MM/DD/YYYY): ", key = 'start_date')
     edate = st.text_input("Enter end date (MM/DD/YYYY): ", key = 'end_date')
-    st.form_submit_button("Fetch Data")
+    submitted = st.form_submit_button("Fetch Data")
 
 #Get inputs before splitting the code
 #st.title("Stock Data Fetcher")
@@ -31,17 +31,19 @@ with st.form('ticker_form'):
 #sdate = st.text_input("Enter start date (MM/DD/YYYY): ", key = 'start_date')
 #edate = st.text_input("Enter end date (MM/DD/YYYY): ", key = 'end_date')
 
-if ticker and sdate and edate:
-    try:
-        smonth, sday, syear = map(int, sdate.split('/'))
-        emonth, eday, eyear = map(int, edate.split('/'))
-        df = save_to_csv_from_google(ticker, syear, smonth, sday, eyear, emonth, eday)
-        st.dataframe(df)
-        fig = px.line(df, x = df.index, y = df.High) #Reference them like this or like df['Close'] but refer to index this way
-        st.plotly_chart(fig)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-elif not ticker or not sdate or not edate:
-    st.warning("Please complete all fields to fetch data.")
+
+if submitted:
+    if ticker and sdate and edate:
+        try:
+            smonth, sday, syear = map(int, sdate.split('/'))
+            emonth, eday, eyear = map(int, edate.split('/'))
+            df = save_to_csv_from_google(ticker, syear, smonth, sday, eyear, emonth, eday)
+            st.dataframe(df)
+            fig = px.line(df, x = df.index, y = df.High) #Reference them like this or like df['Close'] but refer to index this way
+            st.plotly_chart(fig)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    elif not ticker or not sdate or not edate:
+        st.warning("Please complete all fields to fetch data.")
 
 
