8115b3c0e91d5872e8cbbb2b42b8f8114781fbac
julianalexgo <julianalexgo@gmail.com>
2025-06-07 12:01:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,18 @@
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as web
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import datetime as dt
 import mplfinance as mpf
 import plotly.express as px
 
-# Streamlit for web app
+#Streamlit for web app
 import streamlit as st
 
+#Streamlit configuration
+st.set_page_config(page_title = "Fetch Stocks")
+
 #Function to Save to CSV
-def save_to_csv_from_google(ticker, syear, smonth, sday, eyear, emonth, eday):
+def fetch_stocks(ticker, syear, smonth, sday, eyear, emonth, eday):
     start = dt.datetime(syear, smonth, sday)
     end = dt.datetime(eyear, emonth, eday)
     df = web.DataReader(ticker, 'stooq', start, end)
@@ -31,19 +32,19 @@ with st.form('ticker_form'):
 #sdate = st.text_input("Enter start date (MM/DD/YYYY): ", key = 'start_date')
 #edate = st.text_input("Enter end date (MM/DD/YYYY): ", key = 'end_date')
 
-
 if submitted:
+    st.success(f"Fetching data for {ticker} from {sdate} to {edate}...")
     if ticker and sdate and edate:
         try:
             smonth, sday, syear = map(int, sdate.split('/'))
             emonth, eday, eyear = map(int, edate.split('/'))
-            df = save_to_csv_from_google(ticker, syear, smonth, sday, eyear, emonth, eday)
+            df = fetch_stocks(ticker, syear, smonth, sday, eyear, emonth, eday)
             st.dataframe(df)
             fig = px.line(df, x = df.index, y = df.High) #Reference them like this or like df['Close'] but refer to index this way
             st.plotly_chart(fig)
         except Exception as e:
             print(f"An error occurred: {e}")
     elif not ticker or not sdate or not edate:
-        st.warning("Please complete all fields to fetch data.")
+        st.warning("Please enter a ticker symbol and dates to fetch data.")
 
 
